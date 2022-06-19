4cea97f919c3d65c67ebeda8d8fd3395f5775b1f
Julian Espitia <julicam156@gmail.com>
2022-06-19 17:46:28 -0500

    se trabajo en la interfaz de informacion

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -11,11 +11,18 @@ class WorkerInfoDetails extends StatefulWidget {
   }) : super(key: key);
   final WorkerDetails workers;
   final CategoryDetails categories;
+
   @override
   State<WorkerInfoDetails> createState() => _WorkerInfoDetailsState();
 }
 
 class _WorkerInfoDetailsState extends State<WorkerInfoDetails> {
+  final String title1 = 'General',
+      subtitle1 =
+          '                                something here, something here, something here, something here, something here, something here';
+  final String title2 = 'Attiude',
+      subtitle2 =
+          'something here, something here, something here, something here, something here, something here';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +33,132 @@ class _WorkerInfoDetailsState extends State<WorkerInfoDetails> {
           child: Column(
             children: [
               RowWorkerInfo(context),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset(widget.workers.image),
+                  ),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        spreadRadius: 3,
+                        blurRadius: 7,
+                        offset: const Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Text(
+                  widget.workers.name,
+                  style: GoogleFonts.openSans(
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Container(
+                  height: 481,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0, left: 10.0),
+                        child: Text(
+                          'Stats',
+                          style: GoogleFonts.openSans(
+                            textStyle: const TextStyle(
+                              fontSize: 25,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0, left: 10.0),
+                        child: StatsWidget(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0, left: 10.0),
+                        child: Text(
+                          'Last Updates',
+                          style: GoogleFonts.openSans(
+                            textStyle: const TextStyle(
+                              fontSize: 25,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Card(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Card(),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Center Card() {
+    return Center(
+      child: Container(
+        height: 100,
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.lightBlue.shade300,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: ListTile(
+          title: Text(
+            title1,
+            style: GoogleFonts.openSans(
+              textStyle: const TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          subtitle: Text(
+            subtitle2,
+            style: GoogleFonts.openSans(
+              textStyle: const TextStyle(
+                fontSize: 15,
+                color: Colors.black,
+              ),
+            ),
           ),
         ),
       ),
@@ -75,6 +207,44 @@ class _WorkerInfoDetailsState extends State<WorkerInfoDetails> {
             color: Colors.black,
             size: 35.0,
           ),
+        ),
+      ],
+    );
+  }
+
+  Row StatsWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Column(
+          children: const [
+            Icon(
+              Icons.access_alarm_rounded,
+              size: 50,
+              color: Colors.red,
+            ),
+            Text('43 likes')
+          ],
+        ),
+        Column(
+          children: const [
+            Icon(
+              Icons.access_alarm_rounded,
+              size: 50,
+              color: Colors.red,
+            ),
+            Text('21 thanks')
+          ],
+        ),
+        Column(
+          children: const [
+            Icon(
+              Icons.access_alarm_rounded,
+              size: 50,
+              color: Colors.red,
+            ),
+            Text('43 credits')
+          ],
         ),
       ],
     );
